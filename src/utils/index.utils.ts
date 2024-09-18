b74915838247dee678b2d80535f4ba533f8b28cd
abbesm0hamed <abbesmohamed717@gmail.com>
2024-09-18 19:43:46 +0100

    update(url): vercel config and url on prod

--- src/utils/index.utils.ts
+++ src/utils/index.utils.ts
@@ -4,7 +4,7 @@ export const getBaseUrl = (path: string): string => {
   if (import.meta.env.VITE_NODE_ENV == 'development') {
     baseUrl = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_PORT}` || 'http://localhost:5000';
   } else {
-    baseUrl = `${import.meta.env.VITE_BACKEND_URL}` || "https://dujap-cars.vercel.app";
+    baseUrl = `${import.meta.env.VITE_BACKEND_URL}/v1` || "https://dujap-cars.vercel.app";
   }
 
   // Ensure baseUrl doesn't end with a slash and path starts with a slash
